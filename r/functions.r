d45e0b1304868c53b8188d5c930f7aa805044ed3
malexan <alexander@matrunich.com>
2014-09-01 13:25:50 +0400

    str_extract_digs() vectorized

--- r/functions.r
+++ r/functions.r
@@ -1,7 +1,7 @@
 str_extract_digs <- function(s) {
+  if(length(s) > 1) return(lapply(s, str_extract_digs))
   library(stringr)
   exp <- '([0-9]+)'
   charlist <- str_extract_all(s, exp)
-  digs <- as.numeric(unlist(charlist))
-  digs
-} 
+  as.integer(unlist(charlist))
+}
