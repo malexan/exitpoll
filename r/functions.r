5bb0232b50d634ac564582941b9a36bfe789f749
malexan <alexander@matrunich.com>
2014-09-01 14:54:53 +0400

    str_extract_digs() renamed. melt_text() added

--- r/functions.r
+++ r/functions.r
@@ -1,7 +1,14 @@
-str_extract_digs <- function(s) {
+str_extract_numbs <- function(s) {
   if(length(s) > 1) return(lapply(s, str_extract_digs))
   library(stringr)
   exp <- '([0-9]+)'
   charlist <- str_extract_all(s, exp)
   as.integer(unlist(charlist))
+}
+
+melt_text <- function(id, text) {
+  numbers <- str_extract_numbs(text)
+  data.frame(id = id,
+             position = seq_along(numbers),
+             value = numbers)
 }
