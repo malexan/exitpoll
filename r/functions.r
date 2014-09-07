a3879ba29cfa3f0c97b1cf850eef5d6252ecea23
malexan <alexander@matrunich.com>
2014-09-07 16:22:41 +0400

    str_extract_digs corrected to ..._numbs in vectorization section

--- r/functions.r
+++ r/functions.r
@@ -2,7 +2,7 @@ source('r/smstower.r')
 
 
 str_extract_numbs <- function(s) {
-  if(length(s) > 1) return(lapply(s, str_extract_digs))
+  if(length(s) > 1) return(lapply(s, str_extract_numbs))
   library(stringr)
   exp <- '([0-9]+)'
   charlist <- str_extract_all(s, exp)
