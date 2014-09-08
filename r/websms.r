f8af931cbc3e78fa35599e2567b024a40bb4adc8
malexan <alexander@matrunich.com>
2014-09-08 14:36:10 +0400

    Have to change sms-provider

---
+++ r/websms.r
@@ -0,0 +1,24 @@
+websms_getdata <- function(user, passwd, startdate, enddate) {
+  library(httr)
+  library(XML)
+  cafile <- system.file("CurlSSL", "cacert.pem", package = "RCurl")
+  
+  url <- 'https://websms.ru/'
+  path <- 'http_feedback.asp'
+  r <- GET(url, path = path, query = list(http_username = user,
+                                          http_password = passwd,
+                                          startdate = startdate,
+                                          enddate = enddate,
+                                          format = "xml"),
+           config(cainfo = cafile, ssl.verifypeer = FALSE))
+  
+  r <- xmlToList(xmlParse(r))
+  r <- dplyr::rbind_all(lapply(r, function(x) {
+    data.frame(time = x$.attrs[['sms_date']], 
+               text = x$text,
+               agent = x$.attrs[['telnum']],
+               stringsAsFactors = F)
+  }))
+  r$time <- lubridate::dmy_hms(r$time)
+  r  
+}
