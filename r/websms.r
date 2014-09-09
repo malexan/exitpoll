926d55bc18d6011b2b8332a7fc22176b7ad422c8
Alexander Matrunich <alexander@matrunich.com>
2014-09-09 12:47:26 +0400

    If no new data, empty data.frame returned

--- r/websms.r
+++ r/websms.r
@@ -14,6 +14,7 @@ websms_getdata <- function(user, passwd, startdate, enddate) {
                   encoding = "CP1251"))
   
   r <- xmlToList(xmlParse(r))
+  if (r[[1]][[2]] == "no records found") return (data.frame())
   r <- dplyr::rbind_all(lapply(r, function(x) {
     data.frame(time = x$.attrs[['sms_date']], 
                text = x$text,
@@ -22,4 +23,26 @@ websms_getdata <- function(user, passwd, startdate, enddate) {
   }))
   r$time <- lubridate::dmy_hms(r$time)
   r  
+}
+
+
+websms_sendsms <- function(text, recipient, sender, user, passwd) {
+  library(httr)
+  library(stringr)
+  
+  if(length(recipient) > 1) recipient <- str_c(recipient, collapse = ',')
+  
+  cafile <- system.file("CurlSSL", "cacert.pem", package = "RCurl")
+  
+  url <- 'https://websms.ru/'
+  path <- 'http_in5.asp'
+  
+  r <- GET(url, path = path, query = list(http_username = user,
+                                          http_password = passwd,
+                                          fromPhone = sender,
+                                          Phone_list = recipient,
+                                          Message = text),
+           config(cainfo = cafile, ssl.verifypeer = FALSE,
+                  encoding = "CP1251"))
+  r
 }
